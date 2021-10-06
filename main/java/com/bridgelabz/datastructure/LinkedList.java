8a5eefd3deb231fc47382a22e0a5e5af890b7b22
shindeanant <shindeanant1997@gmail.com>
2021-10-06 13:49:36 +0530

    ADD:Welcome to linked list program

---
+++ main/java/com/bridgelabz/datastructure/LinkedList.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.datastructure;
+
+
+public class LinkedList 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to linkedlist" );
+    }
+}
